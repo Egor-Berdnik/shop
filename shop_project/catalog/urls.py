1d020a23154b80164341455cfcb797b2d8fcb4a8
Egor-Berdnik <goldenbereg97@gmail.com>
2023-04-11 19:51:39 +0300

    add user cart functionality

--- shop_project/catalog/urls.py
+++ shop_project/catalog/urls.py
@@ -1,20 +1,23 @@
 from django.urls import path
 from catalog.views import ProducersListView, DiscountsListView, PromocodesListView, \
-    ProductsListView, CategoriesListView, CategoryProductsView, ProducerProductView, DiscountProductsView
+    ProductsListView, CategoriesListView, CategoryProductsView, ProducerProductView, DiscountProductsView, BasketView
 # CategoryView
 
 urlpatterns = [
-    path('categories/', CategoriesListView.as_view()),
+
+    #Client
+    path('categories/', CategoriesListView.as_view(), name='category'),
     path('categories/<int:category_id>/', CategoryProductsView.as_view(), name='category-products'),
 
-    path('producers/', ProducersListView.as_view()),
+    path('producers/', ProducersListView.as_view(), name='producer'),
     path('producers/<int:producer_id>/', ProducerProductView.as_view(), name='producer-products'),
 
-    path('discounts/', DiscountsListView.as_view()),
+    path('discounts/', DiscountsListView.as_view(), name='discount'),
     path('discounts/<int:discount_id>/', DiscountProductsView.as_view(), name='discount-products'),
 
-    path('promocodes/', PromocodesListView.as_view()),
-    path('products/', ProductsListView.as_view())
+    path('promocodes/', PromocodesListView.as_view(), name='promocodes'),
+    path('products/', ProductsListView.as_view(), name='products'),
+
+    #Customer
+    path('cart/', BasketView.as_view(), name='user-basket')
 ]
-
-
--- shop_project/catalog/views.py
+++ shop_project/catalog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from catalog.models import Category, Producer, Discount, Promocode, Product
+from catalog.models import Category, Producer, Discount, Promocode, Product, Basket
 from rest_framework.generics import ListAPIView
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -7,7 +7,9 @@ from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework.mixins import ListModelMixin
 from catalog.serializers import CategorySerializer, ProducerSerializer, DiscountSerializer, PromocodeSerializer, \
-    ProductSerializer
+    ProductSerializer, BasketSerializer, AddProductSerializer, DeleteProductSerializer
+from django.db.models import F
+from django.shortcuts import get_object_or_404
 
 
 class CategoriesListView(ListAPIView):
@@ -87,3 +89,53 @@ class ProductsListView(ListAPIView):
     queryset = Product.objects.all()
     permission_classes = (AllowAny, )
     serializer_class = ProductSerializer
+
+
+class BasketView(APIView):
+    permission_classes = (IsAuthenticated, )
+
+    def get(self, request):
+        user = request.user
+        basket = Product.objects.prefetch_related("basket_set").filter(basket__user=user).values(
+            "name", "price", "discount", number_of_items=F("basket__count"), discount_percent=F("discount__percent"),
+            discount_date_end=F("discount__date_end"), discount_name=F("discount__name")
+        )
+        serializer = BasketSerializer({'products': basket})
+
+        return Response(serializer.data)
+
+    def post(self, request):
+        input_serializer = AddProductSerializer(data=request.data)
+        input_serializer.is_valid(raise_exception=True)
+
+        product = get_object_or_404(Product, id=input_serializer.data.get('product_id'))
+
+        # basket = Basket(user=request.user,
+        #                 product=product,
+        #                 count=input_serializer.data.get('number_of_items)'))
+        # basket.save()
+
+        basket_object, created = Basket.objects.get_or_create(user=request.user, product=product)
+
+        if basket_object.count:
+            basket_object.count += input_serializer.data.get('number_of_items')
+        else:
+            basket_object.count = input_serializer.data.get('number_of_items')
+
+        if basket_object.count <= 0:
+            basket_object.delete()
+        else:
+            basket_object.save()
+
+        return Response()
+
+    def delete(self, request):
+        input_serializer = DeleteProductSerializer(data=request.data)
+        input_serializer.is_valid(raise_exception=True)
+
+        product = get_object_or_404(Product, id=input_serializer.data['product_id'])
+        # product = Product.object.get(id=input_serializer.data['product_id'])
+
+        Basket.objects.get(user=request.user, product=product).delete()
+
+        return Response()
